aba6c7db7212773b25b8bf038dd407121c41c49f
Dimitry Duarte <dimitry@startpdv.com.br>
2018-03-28 09:44:51 -0300

    Subindo Projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# operadoraGoogleExtension
---
+++ content.js
@@ -0,0 +1,10 @@
+//alert("Hello from your Chrome extension!");
+/*$(function () {
+    $('#pesquisar').on('click', function () {
+        var telefone = $('#telefone').val();
+        var request = "http://192.168.0.239/operadora.php?tel1="+telefone;
+        $.post(request, function(retorno){
+        	('#resposta').val() = retorno;
+        });
+    });
+});*/
