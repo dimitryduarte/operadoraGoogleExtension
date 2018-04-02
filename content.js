ecdacaea98e0ebd71dd4b4c1f29e122794f27ca1
Dimitry Duarte <dimitry@startpdv.com.br>
2018-04-02 12:52:53 -0300

    Modificado o link para rede interna

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
-//alert("Hello from your Chrome extension!");
-/*$(function () {
-    $('#pesquisar').on('click', function () {
-        var telefone = $('#telefone').val();
-        var request = "http://192.168.0.239/operadora.php?tel1="+telefone;
-        $.post(request, function(retorno){
-        	('#resposta').val() = retorno;
-        });
-    });
-});*/
+	function pesquisar(){
+		var telefone = $('#telefone').val();
+		var request = "http://192.168.0.239/operadora.php?tel1="+telefone;
+		$.post(request, function(retorno){
+			objretorno = json.decode(retorno);
+			$('#resposta').val() = objretorno;
+		});
+	}
